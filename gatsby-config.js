ad158fcb62ca7e2b902cd56b9f399632bfa84100
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-08-04 11:44:05 +0100

    updated metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    title: `Dara Kelly`,
+    description: `A youth outreach programme`,
+    author: `@Dara_Kelly`,
+    siteUrl: `https://darakelly.xyz/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
