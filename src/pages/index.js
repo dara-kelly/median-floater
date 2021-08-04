efcbe64402b41612ef9a674491e604f5ce41dc44
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-08-04 11:45:45 +0100

    updated text

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,10 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
+    <h1>Hide if you think you can</h1>
   <div class="full">
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <p>This is the site while it is being built.</p>
+    <p>Don't get caught.</p>
     <StaticImage
       src="../images/gatsby-astronaut.png"
       width={300}
@@ -23,7 +23,6 @@ const IndexPage = () => (
   </div>
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </p>
   </Layout>
 )
