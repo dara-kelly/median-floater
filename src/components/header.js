e5aa75cde1f43bf8e426f7901eec0c709d2a94d5
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-08-04 11:24:22 +0100

    semicolons

--- src/components/header.js
+++ src/components/header.js
@@ -5,12 +5,12 @@ import { Link } from "gatsby"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#000`;
-      font-family: `Courier`;
-      font-size: `16px`;
-      color: `#00f`;
-      margin-left: `5rem`;
-      margin-right: `5rem`;
+      background: `#000`
+      font-family: `Courier`
+      font-size: `16px`
+      color: `#00f`
+      margin-left: `5rem`
+      margin-right: `5rem`
     }}
   >
 
