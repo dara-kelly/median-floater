bf72469f0e5a6ad627074513ee4c159550267ffd
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-08-06 11:18:21 +0100

    header

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -1,11 +1,22 @@
 import React from "react"
 import { graphql } from "gatsby"
+import Header from "/src/components/header"
 
 export default function Template({
-  data, // this prop will be injected by the GraphQL query below.
+  data, 
 }) {
-  const { markdownRemark } = data // data.markdownRemark holds your post data
+  const { markdownRemark } = data 
   const { frontmatter, html } = markdownRemark
+  return (
+    <>
+      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+     >
   return (
     <div className="blog-post-container">
       <div className="blog-post">
