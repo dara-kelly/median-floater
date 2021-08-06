71e4daca8e011e0defda128fac3be7a1a676dc46
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-08-06 12:13:36 +0100

    full header

--- src/pages/{MarkdownRemark.frontmatter__slug}.js
+++ src/pages/{MarkdownRemark.frontmatter__slug}.js
@@ -9,7 +9,14 @@ export default function Template({
   const { markdownRemark } = data 
   const { frontmatter, html } = markdownRemark
   return (
-    <Header />
+    <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
+      <div
+        style={{
+          margin: `0 auto`,
+          maxWidth: 960,
+          padding: `0 1.0875rem 1.45rem`,
+        }}
+      >
     <div className="blog-post-container">
       <div className="blog-post">
         <h1>{frontmatter.title}</h1>
