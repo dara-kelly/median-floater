8646239ee460e9b1bf4bf9a1420c9c4628698eea
Dara Kelly <dazk98@gmail.com>
2021-09-16 21:52:03 +0100

    added readinglist

---
+++ src/pages/readinglist.js
@@ -0,0 +1,51 @@
+
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+
+
+const readinglistPage = () => (
+  <Layout>
+    <h1>Reading list</h1>
+<div> 
+    <ul> 
+	<li> Joan Retallack - The Poethical Wager</li>
+    	<li> Donald Davie - Articulating Energy</li>
+    	<li> Hugh Kenner - The Pound Era</li>
+    	<li> Guy Davenport - Geographies of the Imagination</li>
+    	<li> Frank Kermode - Romantic Image</li>
+    	<li> Marjorie Perloff - 21st Century Modernism</li> 
+    	<li> Anne Carson - Eros the Bittersweet</li>
+	<li> Louis-Ferdinand Celine - Journey to the End of Night</li>
+	<li> Alain Robbe-Grillet - The Voyeur</li>
+	<li> Mathias Enard - Zone</li>
+	<li> Kathy Acker - Empire of the Senseless</li>
+	<li> Ingebord Bachmann - Malina</li>
+    </ul>
+	</div>
+
+	<div>
+	<h1>Poetry</h1>
+	<ul>
+		<li>Carson</li>
+		<li>Celan </li>
+		<li>Cesaire </li>
+		<li>Moore </li>
+		<li>Olson </li>
+		<li>Oppen </li>
+		<li>Pound </li>
+		<li>Prynne </li>
+		<li>Riley </li>
+		<li>Sinclair </li>
+		<li>Zukofksy </li>
+	</ul>
+	</div>
+
+	<div>
+		<h1>Fiction</h1>
+	<ul >
+		<li>Bernhard, Thomas - Concrete/The Loser </li>
+		<li>Bolano, Roberto - The Savage Detectives</li>
+		<li>Cohen, Joshua - Witz/Moving Kings </li>
+		<li>Conrad, Joseph - Nostromo </li>
