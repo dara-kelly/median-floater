3d1d0f00275f52123ab270eae815dd73d2627702
Dara Kelly <dazk98@gmail.com>
2021-09-27 21:42:16 +0100

    added readinglist seo

--- src/pages/readinglist.js
+++ src/pages/readinglist.js
@@ -3,7 +3,7 @@ import React from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-
+import Seo from "../components/seo"
 
 const readinglistPage = () => (
   <Layout>
