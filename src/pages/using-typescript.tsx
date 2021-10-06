63a92c76920f1e1c8817a472da030ea526403254
dara-kelly <56123923+dara-kelly@users.noreply.github.com>
2021-10-06 11:11:04 +0100

    Principles for web design

--- src/pages/using-typescript.tsx
+++ src/pages/using-typescript.tsx
@@ -13,28 +13,23 @@ type DataProps = {
 
 const UsingTypescript: React.FC<PageProps<DataProps>> = ({ data, path }) => (
   <Layout>
-    <Seo title="Using TypeScript" />
-    <h1>Gatsby supports TypeScript by default!</h1>
+    <Seo title="New site principles" />
+    <h1>Principles for the new site</h1>
     <p>
-      This means that you can create and write <em>.ts/.tsx</em> files for your
-      pages, components etc. Please note that the <em>gatsby-*.js</em> files
-      (like gatsby-node.js) currently don't support TypeScript yet.
+      <ol>
+        <li>Must support serving markdown pages from Github</li>
+        <li>As much in vanilla HTML/CSS as possible</li>
+        <li>System fonts, if possible</li>
+        <li>Entire site should be as lightweight as possible</li>
+        </ol>
     </p>
     <p>
-      For type checking you'll want to install <em>typescript</em> via npm and
-      run <em>tsc --init</em> to create a <em>.tsconfig</em> file.
+      Current options:
+      <ul>
+        <li>Hugo</li>
+        <li>Vanilla HTML done in pandoc, using a VPS</li>
     </p>
-    <p>
-      You're currently on the page "{path}" which was built on{" "}
-      {data.site.buildTime}.
-    </p>
-    <p>
-      To learn more, head over to our{" "}
-      <a href="https://www.gatsbyjs.com/docs/typescript/">
-        documentation about TypeScript
-      </a>
-      .
-    </p>
+    
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
